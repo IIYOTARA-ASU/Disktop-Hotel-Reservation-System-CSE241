0de32d1bf4b431906de7173abfe50dec77358d21
Animtlz <animtlz666@gmail.com>
2026-04-05 22:43:38 +0200

    Rooms and Database Classes Complete (i think)

--- src/main/java/com/mycompany/desktophotelreservationsystem/DataBase.java
+++ src/main/java/com/mycompany/desktophotelreservationsystem/DataBase.java
@@ -1,4 +1,14 @@
 package com.mycompany.desktophotelreservationsystem;
 public class DataBase {
+    ArrayList<Guest> guests = new ArrayList<>();
+    ArrayList<Room> rooms = new ArrayList<>();
+    ArrayList<Reservation> reservations = new ArrayList<>();
+    ArrayList<Invoice> invoices = new ArrayList<>();
+    ArrayList<Staff> staff = new ArrayList<>();
     
+    guests.add(new Guest());
+    rooms.add(new Room());
+    reservations.add(new Reservation());
+    invoices.add(new Invoice());
+    staff.add(new Admin());
 }
--- src/main/java/com/mycompany/desktophotelreservationsystem/Room.java
+++ src/main/java/com/mycompany/desktophotelreservationsystem/Room.java
@@ -2,5 +2,53 @@
 package com.mycompany.desktophotelreservationsystem;
 
 public class Room {
-    
+	private RoomType type;
+	private Guest roomGuest;
+	private boolean occupied;
+	private int roomNumber;
+
+	Room() {
+		type = new Single();
+		occupier = "Unnamed";
+		occupied = false;
+	}
+
+	Room(RoomType type, Guest roomGuest, boolean occupied) {
+		this.type = type;
+		this.roomGuest = roomGuest;
+		this.occupied = occupied;
+	}
+
+	public Roomtype getRoomType() {
+		return type;
+	}
+
+	public Guest getroomGuest() {
+		return roomGuest;
+	}
+
+	public boolean getOccupied() {
+		return occupied;
+	}
+
+	public int getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomType(RoomType type) {
+		this.type = type;
+	}
+
+	public void setRoomGuest(Guest roomGuest) {
+		this.roomGuest = roomGuest;
+	}
+
+	public void setOccupied(boolean occupied) {
+		this.occupied = occupied;
+	}
+
+	public void setRoomNumber(int roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
 }
