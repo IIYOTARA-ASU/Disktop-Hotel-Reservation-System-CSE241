31d3e468ce8398eeaca0ff1c9808dcbd4169fdb6
Animtlz <animtlz666@gmail.com>
2026-04-05 23:49:25 +0200

    retreived Database

--- src/main/java/com/mycompany/desktophotelreservationsystem/DataBase.java
+++ src/main/java/com/mycompany/desktophotelreservationsystem/DataBase.java
@@ -1,5 +1,17 @@
 package com.mycompany.desktophotelreservationsystem;
 import java.util.ArrayList;
 public class DataBase {
+    ArrayList<Guest> guests = new ArrayList<>();
+    ArrayList<Room> rooms = new ArrayList<>();
+    ArrayList<Reservation> reservations = new ArrayList<>();
+    ArrayList<Invoice> invoices = new ArrayList<>();
+    ArrayList<Staff> staff = new ArrayList<>();
     
+    Database(){
+    guests.add(new Guest());
+    rooms.add(new Room());
+    reservations.add(new Reservation());
+    invoices.add(new Invoice());
+    staff.add(new Admin());
+    }
 }
