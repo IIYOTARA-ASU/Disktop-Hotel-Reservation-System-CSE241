6e784af60a283467bff1e5bcadd06905251eecd9
IIYOTARA-ASU <yousefgogo990@gmail.com>
2026-04-05 18:54:09 +0200

    startingPoint

---
+++ src/main/java/com/mycompany/desktophotelreservationsystem/DesktopHotelReservationSystem.java
@@ -0,0 +1,8 @@
+package com.mycompany.desktophotelreservationsystem;
+
+public class DesktopHotelReservationSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
