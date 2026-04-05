7c820bf0eec6feff1f8cf31d9746602233ba5a0a
IIYOTARA-ASU <yousefgogo990@gmail.com>
2026-04-05 22:00:55 +0200

    DataBase added

--- src/main/java/com/mycompany/desktophotelreservationsystem/Amenity.java
+++ src/main/java/com/mycompany/desktophotelreservationsystem/Amenity.java
@@ -1,5 +1,35 @@
 
 package com.mycompany.desktophotelreservationsystem;
 public class Amenity {
-    // i am playing with YOUR ball
+double price;
+String Name;
+int number;
+Amenity(String s,double p,int no)
+{
+    Name=s;
+    price=p;
+    number=no;
+}
+double getprice(){
+    return this.price;
+}
+int getNumber()
+{
+    return this.number;
+}
+String getName()
+{
+    return this.Name;
+}
+void setName(String s)
+{
+    this.Name=s;
+}
+void setNumber(int n)
+{
+    this.number=n;
+}
+void setPrice(double p){
+    this.price=p;
+}
 }
--- src/main/java/com/mycompany/desktophotelreservationsystem/DataBase.java
+++ src/main/java/com/mycompany/desktophotelreservationsystem/DataBase.java
@@ -1,16 +1,4 @@
 package com.mycompany.desktophotelreservationsystem;
 public class DataBase {
-    ArrayList<Guest> guests = new ArrayList<>();
-    ArrayList<Room> rooms = new ArrayList<>();
-    ArrayList<Reservation> reservations = new ArrayList<>();
-    ArrayList<Invoice> invoices = new ArrayList<>();
-    ArrayList<Staff> staff = new ArrayList<>();
     
-    Database(){
-    guests.add(new Guest());
-    rooms.add(new Room());
-    reservations.add(new Reservation());
-    invoices.add(new Invoice());
-    staff.add(new Admin());
-    }
 }
