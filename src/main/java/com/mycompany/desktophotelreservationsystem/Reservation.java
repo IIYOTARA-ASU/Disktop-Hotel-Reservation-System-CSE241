be025a089e810482c91ad193f945445bbc46c874
Baraa Khaled <4baraakhaled@gmail.com>
2026-04-05 23:04:23 +0200

    Update Reservation class

--- src/main/java/com/mycompany/desktophotelreservationsystem/Reservation.java
+++ src/main/java/com/mycompany/desktophotelreservationsystem/Reservation.java
@@ -1,6 +1,71 @@
-
 package com.mycompany.desktophotelreservationsystem;
+import java.util.Date;
 
 public class Reservation {
+    public enum ReservationStatus { PENDING, CONFIRMED, CANCELLED, COMPLETED }
     
+    Reservation() {
+    }
+    
+    Reservation(Guest guest, Room room, Date checkInDate, Date checkOutDate) {
+        this.guest = guest;
+        this.room = room;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+        this.reservationStatus = ReservationStatus.PENDING;
+    }
+    
+    private Guest guest;
+    private Room room;
+    private ReservationStatus reservationStatus;
+    private Date checkInDate;
+    private Date checkOutDate;
+
+    // ###############################  GETTERS  ###############################
+    public Guest getGuest() {
+        return guest;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public ReservationStatus getReservationStatus() {
+        return reservationStatus;
+    }
+
+    public Date getCheckInDate() {
+        return checkInDate;
+    }
+
+    public Date getCheckOutDate() {
+        return checkOutDate;
+    }
+
+
+    // ###############################  SETTERS  ###############################
+    public void setGuest(Guest guest) {
+        this.guest = guest;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public void setReservationStatus(ReservationStatus reservationStatus) {
+        this.reservationStatus = reservationStatus;
+    }
+
+    public void setCheckInDate(Date checkInDate) {
+        if (checkInDate != null && checkInDate.before(checkOutDate)) {
+            this.checkInDate = checkInDate;
+        }
+    }
+
+    public void setCheckOutDate(Date checkOutDate) {
+        if (checkOutDate != null && checkOutDate.after(checkInDate)) {
+            this.checkOutDate = checkOutDate;
+        }
+    }
+
 }
