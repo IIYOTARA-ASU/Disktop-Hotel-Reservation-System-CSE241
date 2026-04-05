e1dd36996b0380b9826100142e53ccc341cca075
IIYOTARA-ASU <yousefgogo990@gmail.com>
2026-04-05 19:29:12 +0200

    StartGit

--- src/main/java/com/mycompany/desktophotelreservationsystem/DesktopHotelReservationSystem.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.desktophotelreservationsystem;
-
-public class DesktopHotelReservationSystem {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
