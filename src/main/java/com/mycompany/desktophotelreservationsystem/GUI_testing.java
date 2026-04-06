a2fa38aa28cfab042691136e226267175d39c692
KEMETIA <hxid5907@gmail.com>
2026-04-06 22:33:23 +0200

    fixed amenities and package error

---
+++ src/main/java/com/mycompany/desktophotelreservationsystem/GUI_testing.java
@@ -0,0 +1,37 @@
+package com.mycompany.desktophotelreservationsystem;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+    public class GUI_testing extends Application {
+
+        @Override
+        public void start(Stage primaryStage) {
+            // Create a button and set its text
+            Button btn = new Button();
+            btn.setText("Say 'Hello World'");
+
+            // Define an action for the button
+            btn.setOnAction(event -> System.out.println("Hello World!"));
+
+            // Add the button to a layout pane
+            StackPane root = new StackPane();
+            root.getChildren().add(btn);
+
+            // Create a scene with the layout pane, setting dimensions
+            Scene scene = new Scene(root, 300, 250);
+
+            // Configure the stage (the main window)
+            primaryStage.setTitle("Hello World!");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }
+
+        public static void main(String[] args) {
+            launch(args);
+        }
+    }
+
