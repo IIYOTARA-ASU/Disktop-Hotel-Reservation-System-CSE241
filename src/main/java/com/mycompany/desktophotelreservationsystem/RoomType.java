d0d598311b0bef916eb6abcf792a782eb79e2b73
Animtlz <animtlz666@gmail.com>
2026-04-07 23:21:15 +0200

    added roomtype

--- src/main/java/com/mycompany/desktophotelreservationsystem/RoomType.java
+++ src/main/java/com/mycompany/desktophotelreservationsystem/RoomType.java
@@ -1,3 +1,16 @@
 package com.mycompany.desktophotelreservationsystem;
 public class RoomType {
+	enum roomtype{Single, Double, Suite};
+	roomtype roomType;
+	RoomType(String type){
+		switch(type) {
+		
+		case "Single" : roomType = roomtype.Single; break;
+		case "Double" : roomType = roomtype.Double; break;
+		case "Suite" : roomType = roomtype.Suite; break;
+		
+		}
+		
+	}
+	
 }
