bb192cde21455e724e2e119c6d9d969269125454
Baraa Khaled <4baraakhaled@gmail.com>
2026-04-07 22:52:11 +0200

    Fixing lots of shit

--- src/main/java/com/mycompany/desktophotelreservationsystem/Staff.java
+++ src/main/java/com/mycompany/desktophotelreservationsystem/Staff.java
@@ -1,7 +1,7 @@
 package com.mycompany.desktophotelreservationsystem;
 public abstract class Staff extends User {
     Staff(DataBase dataBase){
-       super(dataBase);
+        super(dataBase);
     }
     public enum roll {ADMIN, RECEPTIONIST}
     int workingHours;
