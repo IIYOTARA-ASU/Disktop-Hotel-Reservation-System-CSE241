2e95e3621c32e942290f4462f442d9e95359fdda
Animtlz <animtlz666@gmail.com>
2026-04-07 23:07:04 +0200

    Gui can now work anywhere

--- src/main/java/com/mycompany/desktophotelreservationsystem/Main.java
+++ src/main/java/com/mycompany/desktophotelreservationsystem/Main.java
@@ -2,15 +2,15 @@ package com.mycompany.desktophotelreservationsystem;
 public class Main {
     public static void main(String args[]){
         System.out.println("Hello, world!");
-        DataBase.
-        Guest guest = new Guest(dataBase);
-        Receptionist receptionist = new Receptionist(dataBase);
-        Room room = new Room();
-        Admin admin = new Admin(dataBase);
-        admin.createRoom(room);
-        admin.deleteRoom(room);
-        guest.register();
-        guest.login();
+       // DataBase.
+       // Guest guest = new Guest(dataBase);
+       // Receptionist receptionist = new Receptionist(dataBase);
+       // Room room = new Room();
+       // Admin admin = new Admin(dataBase);
+       //admin.createRoom(room);
+       // admin.deleteRoom(room);
+       // guest.register();
+       // guest.login();
 
     }
 }
