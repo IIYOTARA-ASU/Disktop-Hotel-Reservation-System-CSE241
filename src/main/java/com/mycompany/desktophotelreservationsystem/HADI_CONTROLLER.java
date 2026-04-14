3834b23d6b3a2ec4c319ab4e0c3022ff5a1f4258
KEMETIA <hxid5907@gmail.com>
2026-04-14 07:04:10 +0200

    CHECK IF ANYTHING IS WRONG

--- src/main/java/com/mycompany/desktophotelreservationsystem/HADI_CONTROLLER.java
+++ src/main/java/com/mycompany/desktophotelreservationsystem/HADI_CONTROLLER.java
@@ -7,15 +7,15 @@ import javafx.scene.image.ImageView;
 public class HADI_CONTROLLER {
 
     @FXML
-    private Label Adminid; // Added 'private' and ensured it matches fx:id
+    private Label recepname;
+    // Added 'private' and ensured it matches fx:id
 
-    User user = new User("hadi", "123");
 
     @FXML
     public void initialize() {
         // Now Adminid will not be null
-        if (Adminid != null) {
-            Adminid.setText(user.getUserName());
+        if ( recepname!= null) {
+           recepname.setText(DataBase.people.get(DataBase.people.size()).getUserName());
         }
     }
 }
