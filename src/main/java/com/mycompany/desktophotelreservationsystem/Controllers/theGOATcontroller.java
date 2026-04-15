59a87c99e2e20f56aebeafb730a25ec32e45fe5e
Animtlz <animtlz666@gmail.com>
2026-04-15 23:28:26 +0200

    Oh yeaaaa

--- src/main/java/com/mycompany/desktophotelreservationsystem/Controllers/theGOATcontroller.java
+++ src/main/java/com/mycompany/desktophotelreservationsystem/Controllers/theGOATcontroller.java
@@ -23,9 +23,14 @@ public class theGOATcontroller {
 
     @FXML
     private Label dynamicText; 
+    @FXML
+    private Label amenityNameError;
+    @FXML
+    private Label amenityPriceError;
     boolean amenities = false;
     public theGOATcontroller() {}
 
+    
     String s = "Baller";
 
     @FXML
@@ -35,39 +40,60 @@ public class theGOATcontroller {
         } else {
             System.out.println("No dynamicText label found on this screen. Skipping text update.");
         }
+    	
+    	if(amenityNameError != null) {
+    		amenityNameError.setText("Enter name");
+    	}
+    	
+    	if(amenityPriceError != null) {
+    		amenityPriceError.setText("Enter price");
+    	}
+    }
+    
+    @FXML void loadScreen(String path, ActionEvent e) {
+  	  try {
+			root = FXMLLoader.load(getClass().getResource(path));
+		  } catch (IOException e1) {
+			// TODO Auto-generated catch block
+			  System.out.println("Ballersssss");
+			e1.printStackTrace();
+		  }
+  	  stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+  	  scene = new Scene(root);
+  	  stage.setScene(scene);
+  	  stage.show();    
     }
     
     @FXML
     public void toAmenities(ActionEvent e) {
-    	  try {
-			root = FXMLLoader.load(getClass().getResource("/adminAmenities.fxml"));
-		  } catch (IOException e1) {
-			// TODO Auto-generated catch block
-			  System.out.println("Ballersssss");
-			e1.printStackTrace();
-		  }
-    	  stage = (Stage)((Node)e.getSource()).getScene().getWindow();
-    	  scene = new Scene(root);
-    	  stage.setScene(scene);
-    	  stage.show();    
+    	loadScreen("/adminAmenities.fxml",e);
     }
 	
     @FXML
     public void toAdmin(ActionEvent e) {
-    	  try {
-			root = FXMLLoader.load(getClass().getResource("/theGoat.fxml"));
-		  } catch (IOException e1) {
-			// TODO Auto-generated catch block
-			  System.out.println("Ballersssss");
-			e1.printStackTrace();
-		  }
-    	  stage = (Stage)((Node)e.getSource()).getScene().getWindow();
-    	  scene = new Scene(root);
-    	  stage.setScene(scene);
-    	  stage.show();    
+    	loadScreen("/theGoat.fxml",e);
+    }
+    
+    @FXML
+    public void toRooms(ActionEvent e) {
+    	loadScreen("/adminRooms.fxml",e);
+    }
+    
+    @FXML
+    public void toRoomTypes(ActionEvent e) {
+    	loadScreen("/adminRoomTypes.fxml",e);
+    }
+    
+    @FXML
+    public void toAddAmenities(ActionEvent e) {
+    	loadScreen("/adminAmenitiesAdd.fxml",e);
     }
     @FXML
     public void up(ActionEvent e) {
         System.out.println("UP");
+    }
+    @FXML
+    public void addAmenity() {
+    	
     }
 }
